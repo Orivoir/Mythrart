e51665cedb25f737c62df383b7f9698b9795df82
Orivoir <sam.gabor22@gmail.com>
2026-09-01 19:27:15 +0200

    Delete Test dashboard

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
-import DashboardDev from "./DashboardDev"
 
 export default function DashboardPage() {
-  return <DashboardDev />
+  return (
+    <></>
+  )
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,18 @@
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
 import { Container } from "@/components/ui/layout/container"
 import { HeroContent as CTAHeroContent } from "@/components/ui/cta/home/hero-content"
 import { ArgumentsContent as CTAArgumentsContent } from "@/components/ui/cta/home/arguments-content"
 import { FooterText as CTAFooterText } from "@/components/ui/cta/home/footer-text"
 import { Waves } from "@/components/ui/paralax/waves"
+import { authOptions } from "@/lib/auth"
 
 export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    redirect("/dashboard")
+  }
 
   return (
     <div className="relative overflow-hidden">
